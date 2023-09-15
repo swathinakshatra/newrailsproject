15639aa32cebb030ec596bea6b03d16245da4074
swathinakshatra <swathinakshatra485@gmail.com>
2023-09-15 15:33:02 +0530

    changes in redis

--- helpers/redis.js
+++ helpers/redis.js
@@ -1,12 +1,9 @@
 const redis = require('redis');
-const client = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT
+const client = redis.createClient(process.env.REDIS_URL);
+
+client.on('connect', () => {
+    console.log('Connected to Redis...');
 });
-client.connect()
-client.on('connect', err => {
-    console.log('Connected to redis..');
-})
 
 client.on('error', (err) => {
     console.log(err.message);
